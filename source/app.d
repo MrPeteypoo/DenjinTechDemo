2382566741e03801c2927233c1903e73be1e6fd0
Simon Peter Campbell <storm20200@gmail.com>
2017-05-12 11:42:58 +0100

    Update to the latest engine.

--- source/app.d
+++ source/app.d
@@ -3,7 +3,7 @@ import denjin;
 void main()
 {
 	auto engine = Engine();
-    scope (exit) engine.clear();
-    engine.initialise();
-    engine.run();
+    scope (exit) engine.clear;
+    engine.initialise;
+    engine.run;
 }
